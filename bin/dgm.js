e12d9767d10fe2185b8ba56e52fea82bd6a96a6c
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-09-19 16:12:45 +0300

    Initial implementation of 'create' command.

--- bin/dgm.js
+++ bin/dgm.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var commands = require('../lib/commands'),
-  dgm_core = require('../lib/dgm_core'),
-  argv = require('minimist')(process.argv.slice(2)),
-  args = argv._;
+    dgm_core = require('../lib/dgm_core'),
+    argv = require('minimist')(process.argv.slice(2)),
+    args = argv._;
 
 dgm_core.bootstrap(commands, args, argv);
